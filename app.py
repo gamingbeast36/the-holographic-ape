76391f1524a3dda149fac72f8418614cbe953bf7
gamingbeast36 <64628398+gamingbeast36@users.noreply.github.com>
2021-06-07 11:30:47 +0530

    added new template and made changes to ui

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/articles")
+def articles():
+    return render_template("articles.html")
 
-
-
+@app.route("/disease_of_addiction")
+def disease():
+    return render_template("disease_of_addiction.html")
 
 
 
